6162f0da10b5861628a9d5e238e4859821d7dc16
AD1025 <121030788+AD1025@users.noreply.github.com>
2024-08-24 18:43:55 +0530

    Update votingmachine.v

--- votingmachine.v
+++ votingmachine.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.07.2024 21:30:00
-// Design Name: 
-// Module Name: voting
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module buttonControl(
   input clock,
